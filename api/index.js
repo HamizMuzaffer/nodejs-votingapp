502eb9e336c4ff7c5e3377b49756d90fae46279e
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-08-17 02:14:28 +0500

    second commit

--- api/index.js
+++ api/index.js
@@ -2,8 +2,13 @@ require('dotenv').config()
 const express = require("express")
 const PORT = process.env.PORT || 8000;
 const app = express()
+const cookieParser = require('cookie-parser')
+// middlewares 
+
+app.use(cookieParser())
 
 
+// Server Listening
 app.listen(PORT,()=>{
     console.log(`Server Listtening at ${PORT}`)
 })
