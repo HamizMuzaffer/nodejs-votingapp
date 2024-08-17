21cbc55106717a8d9c571c431b06c6c36e827e25
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-08-18 02:45:01 +0500

    models-added

--- api/models/user.js
+++ api/models/user.js
@@ -1,3 +1,17 @@
 const mongoose = require("mongoose")
 
-const userSchema = 
+const userSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    number: { type: Number },
+    address: { type: String, required: true },
+    idCardNo : {type : String , required : true , unique: true},
+    password : {type : String, required : true},
+    role : {type : String , enum : ["voter","admin"], default : "voter"},
+    isVoted : {type : Boolean , default : false}
+})
+
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = User;
