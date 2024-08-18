dd4cedc86683e26a3fb6a7e6a8010db4b9504461
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-08-19 01:27:02 +0500

    user routes added

--- api/models/user.js
+++ api/models/user.js
@@ -11,6 +11,34 @@ const userSchema = mongoose.Schema({
     isVoted : {type : Boolean , default : false}
 })
 
+userSchema.pre("save", function(next){
+    const user = this;
+    if(!user.isModified) return
+    const salt = randomBytes(16).toString();
+    const createHashedPassoword = createHmac("sha256",salt).update(user.password).digest("hex");
+    this.salt = salt;
+    this.password = createHashedPassoword;
+    next()
+
+})
+
+userSchema.static("matchPasswordAndGenerateToken", async function (idCardNo, password) {
+    console.log(`Searching for email: ${email}`); // Log the email being searched
+
+    const user = await this.findOne({idCardNo: new RegExp(`^${idCardNo}$`, 'i')});
+    if (!user) throw new Error("User Not Found");
+
+    const salt = user.salt;
+    const hashedPassword = user.password;
+
+    const userProvidedPassword = createHmac("sha256", salt).update(password).digest("hex");
+
+    if (userProvidedPassword !== hashedPassword) throw new Error("Incorrect Password");
+
+    return user;
+});
+
+
 
 const User = mongoose.model("User", userSchema)
 
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -4,22 +4,103 @@ const router = express.Router()
 const { jwtAuthMiddleware, generateToken } = require("../authentication/auth")
 
 router.post('/signup', async (req, res) => {
-    const data = req.body
-    try {
-       const user = new User(data);
+   const data = req.body
+   try {
+      const user = new User(data);
 
-       const response = await user.save();
+      const response = await user.save();
 
-       const payload = {
-        id : response.id,
-        username : response.username
-       }
+      const payload = {
+         id: response.id,
+         username: response.username
+      }
 
-       const token = generateToken(payload)
-       res.status(200).json({response : response , token : token})
-         
-    } catch (error) {
-       res.status(500).json({ error : "Internal server error" })
-    }
+      const token = generateToken(payload)
+      res.status(200).json({ response: response, token: token })
+
+   } catch (error) {
+      res.status(500).json({ error: "Internal server error" })
+   }
 })
 
+
+router.post("/login", async (req, res) => {
+   try {
+
+      const { idCardNo, password } = req.body; 
+      
+      const user = await User.findOne({ idCardNo : idCardNo, password : password })
+
+      if(!user) return res.status(401).json({error : "user not found"})
+
+         const payload = {
+           id :  user.id,
+         }
+
+         const token = generateToken(token)
+         res.json({ token })
+
+   } catch (error) {
+      res.status(500).json({error : error.message})
+   }
+})
+
+router.get("/profile", jwtAuthMiddleware , async(req,res)=>{
+   try {
+     const userData = req.user;
+     const userId = userData.id;
+   
+     const user = await User.findById({ id : userId })
+     res.status(200).json({ user })
+   
+  } catch (error) {
+   res.status(400).json({ error : "Internal Server Error" })
+  }
+})
+
+router.get("/",jwtAuthMiddleware, async(req,res)=>{
+
+   try {
+
+      const data = User.find();
+      res.status(200).json(data);
+
+   } catch (error) {
+      
+      res.status(500).json({erro : "Internal Server Error"})
+   }
+
+})
+
+router.put("profile/password",jwtAuthMiddleware,async(req,res)=>{
+   try {
+
+      const id = req.user.id;
+
+      const { currentPassword, newPassword } = req.body;
+      
+      const user = User.findById(id)
+
+      if(!user || !await user.comparePassword(currentPassword)){
+         return res.status(401).json({error : "Incorrect password"})
+      }
+
+       user.password = newPassword
+       await user.save()
+       
+      const updatedUserData = req.body;
+
+      const response = await findByIdAndUpdate(id,updatedUserData,{
+         new : true,
+         runValidators : true
+      })
+
+      if(!response) return res.status(401).json({error : "User Not Found"})
+
+      res.status(200).json(response)
+   } catch (error) {
+
+      res.status(500).json({error : error.message})
+
+   }
+})
